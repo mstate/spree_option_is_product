312961748d0893a5c739d7703b47e1e1c4a374af
mike <mstate@gmail.com>
2015-01-07 10:55:02 -0500

    Cleanup comments and force price to be permitted attribute for update to line item.

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,23 +1,9 @@
 Spree::OrderContents.class_eval do
 
-  # def add(variant, quantity=1, currency=nil, shipment=nil, price=nil, force_new_line_item=false)
   def add(variant, quantity=1, options={})
     # force spree to create a new line item for products that are options instead of adding the quantity to those already on the order.
     line_item = options[:force_new_line_item] ? nil : order.find_line_item_by_variant_for_options(variant)
-    # line_item = add_to_line_item(line_item, variant, quantity, currency, shipment, price)
     line_item = add_to_line_item(line_item, variant, quantity, options)
-    # line_item = add_to_line_item(variant, quantity, currency, shipment)
-    # order_updater.update_item_total
-    # order.update_totals
-    # Spree::PromotionHandler::Cart.new(order, line_item).activate
-    # Spree::ItemAdjustments.new(line_item).update
-    # reload_totals
-    # reload_totals
-    # options.has_key?(:shipment) ? shipment.update_amounts : order.ensure_updated_shipments
-    # Spree::PromotionHandler::Cart.new(order, line_item).activate
-    # Spree::ItemAdjustments.new(line_item).update
-    # reload_totals
-    # line_item
     after_add_or_remove(line_item, options)
   end
 
@@ -29,29 +15,14 @@ Spree::OrderContents.class_eval do
 
   def add_to_line_item(line_item, variant, quantity, options={})
     if line_item
-      # line_item.target_shipment = shipment
       line_item.quantity += quantity.to_i
       line_item.currency = currency unless currency.nil?
-      # line_item.save
     else
       opts = { currency: order.currency }.merge ActionController::Parameters.new(options).
-                                              permit(Spree::PermittedAttributes.line_item_attributes)
+                                              permit((Spree::PermittedAttributes.line_item_attributes << :price).uniq)
       line_item = order.line_items.new(quantity: quantity,
                                         variant: variant,
                                         options: opts)
-      # line_item = order.line_items.new(quantity: quantity, variant: variant)
-      # line_item = Spree::LineItem.new(quantity: quantity)
-      # line_item.target_shipment = shipment
-      # line_item.variant = variant
-      # if currency
-      #   line_item.currency = currency unless currency.nil?
-      #   line_item.price    = price || variant.price_in(currency).amount
-      # else
-      #   line_item.price    = price || variant.price
-      # end
-      # order.line_items << line_item
-      # line_item
-      debugger
       line_item.target_shipment = options[:shipment] if options.has_key? :shipment
       line_item.save!
       line_item          
