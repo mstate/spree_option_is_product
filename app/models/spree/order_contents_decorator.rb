7bde0d62d74ca3f1cd26d7eb18483af1d1c7407b
mike <mstate@gmail.com>
2015-02-15 16:08:09 -0500

    Let line_item's logic set the currency if possible.  Only set it here as a backup in case it doesn't get set for some reason.

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -18,18 +18,21 @@ Spree::OrderContents.class_eval do
       line_item.quantity += quantity.to_i
       line_item.currency = currency unless currency.nil?
     else
-      opts = { currency: order.currency }.merge ActionController::Parameters.new(options).
-                                              permit((Spree::PermittedAttributes.line_item_attributes << :price).uniq)
+      opts = ActionController::Parameters.new(options).
+                permit((Spree::PermittedAttributes.line_item_attributes << :price).uniq)
       line_item = order.line_items.new(quantity: quantity,
                                         variant: variant,
                                         options: opts)
       line_item.target_shipment = options[:shipment] if options.has_key? :shipment
-      line_item.save!
-      line_item          
     end
     
-    line_item.save
+    line_item.save!
+
+    # set currency if line_item didn't take care of it.
+    line_item.update_attributes(currency: order.currency) if line_item.currency.nil?
+    
     order.reload
+    
     line_item
   end
 
